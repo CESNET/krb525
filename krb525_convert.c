135242d9cfc6c69d9cfbaf9196363b1bf3ffa3db
Daniel Kouril <kouril@ics.muni.cz>
2017-08-24 12:45:50 +0200

    The client is more resilient to server errors.
    If the renewal operation doesn't succeed against a server, another server
    defined in configuration is tried. It allows the code to cope with temporary
    issues on the server (e.g. when access control can't finished due to
    unavailable DNS). However, since particular errors can't be easily
    distinguished, the change increases the communication to all servers (e.g.
    unauthorized requests will be sent to all servers).

--- krb525_convert.c
+++ krb525_convert.c
@@ -12,6 +12,7 @@
 static char vcid[] = "$Id: krb525_convert.c,v 1.1.1.1 2009/11/13 09:13:02 kouril Exp $";
 #endif /* lint */
 
+#define _GNU_SOURCE
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -30,6 +31,8 @@ static char vcid[] = "$Id: krb525_convert.c,v 1.1.1.1 2009/11/13 09:13:02 kouril
 #include <signal.h>
 #include <pwd.h>
 #include <errno.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 #ifdef HAVE_STDLIB_H
 #include <stdlib.h>
@@ -53,6 +56,11 @@ typedef struct krb525_authdata {
 #endif
 } krb525_authdata;
 
+typedef struct krb525_endpoint_t {
+    char *hostname;
+    unsigned short port;
+} krb525_endpoint_t;
+
 /* Default options if we are authenticating from keytab */
 #define KEYTAB_DEFAULT_TKT_OPTIONS	KDC_OPT_FORWARDABLE
 
@@ -63,8 +71,33 @@ typedef struct krb525_authdata {
 #define GATEWAY_DEFAULT_TKT_OPTIONS	0
 
 
-char krb525_convert_error[255] = "";
+char krb525_convert_error[2048] = "";
 
+static void
+free_strings(char **strings)
+{
+	char **s = strings;
+
+	while (s && *s) {
+		free(*s);
+		s++;
+	}
+
+	free(strings);
+}
+
+static void
+free_endpoints(krb525_endpoint_t **endpoints)
+{
+	krb525_endpoint_t **eps = endpoints;
+
+	while (eps && *eps) {
+		free((*eps)->hostname);
+		free(*eps);
+		eps++;
+	}
+	free(endpoints);
+}
 
 static krb5_error_code
 get_krb525_creds_ccache(krb5_context context,
@@ -138,134 +171,145 @@ krb525_getportbyname(const char *service, const char *proto, short default_port)
   return htons(default_port);
 }
 
-
-static krb5_error_code
-krb525_connect(krb5_context context,
-#ifdef HEIMDAL
-	       krb5_realm          *realm,
-#else
-	       krb5_data           *realm,
-#endif
-               char       **hosts,
-	       int          port,
-	       int         *socket,
-	       char       **server_name)
+static int
+parse_krb525_hosts(char **krb525_hosts, short port, int use_port, krb525_endpoint_t ***endpoints)
 {
-  krb5_error_code retval;
+	krb525_endpoint_t **tmp;
+	krb525_endpoint_t **eps = NULL;
+	int num = 0, ret;
+	char **krb525_host;
+	int krb525_port = -1 ;
+	char *host, *p;
 
-  char *krb525_host = NULL;
-  char **krb525_hosts = NULL;
-  int krb525_host_num = 0;
-  int krb525_port = -1, default_port = -1, use_port = 0;
-  int sock;
- 
-#ifdef HEIMDAL
-  krb5_realm          default_realm;
-#else
-  krb5_data           default_realm;
-#endif
+	eps = calloc(1, sizeof(**eps));
+	if (eps == NULL) {
+		ret = ENOMEM;
+		goto end;
+	}
+	num = 1;
 
-  *socket = -1;
-  *server_name = NULL;
+	for (krb525_host = krb525_hosts; krb525_host && *krb525_host; krb525_host++) {
+		host = *krb525_host;
 
+		if (strncmp(host, "http://", 7) == 0)
+			host += 7;
+		else if (strncmp(host, "http/", 5) == 0)
+			host += 5;
+		else if (strncmp(host, "tcp/", 4) == 0)
+			host += 4;
+		else if (strncmp(host, "udp/", 4) == 0)
+			host += 4;
 
-  if(realm == NULL) {
-  /* Get default realm */
-#ifdef HEIMDAL
-    if (retval = krb5_get_default_realm(context, &default_realm)) 
-#else
-    if (retval = krb5_get_default_realm(context, &(default_realm.data))) 
-#endif
-      {
-	sprintf(krb525_convert_error, "%s while getting default realm\n", 
-		error_message(retval));
-	return(retval);
-      }
-    realm = &default_realm;
-  }
+		krb525_port = -1;
+		p = strchr(host, ':');
+		if (p) {
+			if(use_port)
+				krb525_port = htons(atoi(p+1));
+			*p = 0;
+		}
+		if(port > 0)
+			krb525_port = htons(port);
+		else if(krb525_port <= 0)
+			krb525_port = krb525_getportbyname(KRB525_SERVICE, "tcp", KRB525_PORT);
 
-  /* Get servers for realm - passed as argument, krb525_servers in config file
-     or kdc in config file */
-  if(hosts) {
-    krb525_hosts = hosts;
-  } else {
-#ifdef HEIMDAL
-    krb525_hosts = krb5_config_get_strings(context, NULL, "realms", *realm, "krb525_server", NULL);
-    if(krb525_hosts == NULL) {
-      /* Get list of possible server hosts (same as KDC server hosts) */
-      if (retval = krb5_get_krbhst(context, realm, &krb525_hosts)) {
-	sprintf(krb525_convert_error, "%s while getting server hosts\n",
-		error_message(retval));
-	return(retval);
-      }
-    } else 
-      use_port = 1;
-#else
-    {
-      char *tmp;
+		tmp = realloc(eps, (num + 1) * sizeof(*eps));
+		if (tmp == NULL) {
+			ret = ENOMEM;
+			goto end;
+		}
+		eps = tmp;
+		eps[num - 1] = malloc(sizeof(**eps));
+		if (eps[num - 1] == NULL) {
+			ret = ENOMEM;
+			goto end;
+		}
+		eps[num - 1]->hostname = strdup(host);
+		eps[num - 1]->port = krb525_port;
+		eps[num] = NULL;
+		num++;
+	}
 
-      /* N.B. there's a different location of the directive! */
-      krb5_appdefault_string(context, NULL, realm, "krb525_server", NULL, &tmp);
-      krb525_hosts = &tmp;
-    }
-#endif
-  }
+	ret = 0;
+	*endpoints = eps;
+	eps = NULL;
 
-  /* If no host was found, return error */
-  if (!krb525_hosts || !krb525_hosts[0]) 
-    return(KRB5_KDC_UNREACH);
-
-  /* Get default server port */
-  if(port <= 0)
-    default_port = krb525_getportbyname(KRB525_SERVICE, "tcp", KRB525_PORT);
-
-  /* Try to contact server */
-  for (krb525_host_num = 0; krb525_host = krb525_hosts[krb525_host_num]; krb525_host_num++) {
-
-    if(strncmp(krb525_host, "http://", 7) == 0){
-      krb525_host += 7;
-    } else if(strncmp(krb525_host, "http/", 5) == 0) {
-      krb525_host += 5;
-    } else if(strncmp(krb525_host, "tcp/", 4) == 0){
-      krb525_host += 4;
-    } else if(strncmp(krb525_host, "udp/", 4) == 0) {
-      krb525_host += 4;
-    }
-
-    /* Get server port - from argument, configuration or default */
-    krb525_port = -1;
-    {
-      char *c;
-
-      if(c = strchr(krb525_host, ':')) {
-	if(use_port) 
-	  krb525_port = htons(atoi(c+1));
-	*c = 0;
-      }
-    }
-    if(port > 0)
-      krb525_port = htons(port);
-    else if(krb525_port <= 0)
-      krb525_port = default_port;
-
-    if ((sock = connect_to_server(krb525_host, krb525_port)) >= 0 )
-      /* Success */
-      break;
-    else {
-      fprintf(stderr,"%s while connecting to the server\n",error_message(errno));
-#if 0
-      sprintf(krb525_convert_error, "%s while connecting to the server\n",
-	      error_message(errno));
-      return(errno);
-#endif
-    }
-  }
-
-  *socket = sock;
-  *server_name = krb525_host;
-  return(0);
+end:
+	if (eps)
+		free_endpoints(eps);
+	return ret;
 }
-	       
+
+static int
+get_krb525_endpoints(krb5_context context, short port, char *realm, krb525_endpoint_t ***krb525_endpoints)
+{
+	krb5_error_code retval;
+	char **krb525_hosts = NULL;
+	char *default_realm = NULL;
+	int use_port = 0;
+
+	if(realm == NULL) {
+		retval = krb5_get_default_realm(context, &default_realm);
+		if (retval) {
+			snprintf(krb525_convert_error, sizeof(krb525_convert_error),
+				"%s while getting default realm\n",
+				error_message(retval));
+			return(retval);
+		}
+		realm = default_realm;
+	}
+
+#ifdef HEIMDAL
+	krb525_hosts = krb5_config_get_strings(context, NULL, "realms", realm, "krb525_server", NULL);
+	if (krb525_hosts == NULL) {
+		/* Get list of possible server hosts (same as KDC server hosts) */
+		retval = krb5_get_krbhst(context, &realm, &krb525_hosts);
+		if (retval) {
+			snprintf(krb525_convert_error, sizeof(krb525_convert_error),
+				"%s while getting server hosts\n",
+				error_message(retval));
+			goto end;
+		}
+	} else
+		use_port = 1;
+#else
+	{
+		char *s;
+		krb5_data data_realm;
+
+		data_realm.data = realm;
+		data_realm.length = strlen(realm);
+
+		/* N.B. there's a different location of the directive! */
+		krb5_appdefault_string(context, NULL, &data_realm, "krb525_server", NULL, &s);
+		krb525_hosts = calloc(2, sizeof(*krb525_hosts));
+		if (krb525_hosts == NULL) {
+			retval = ENOMEM;
+			goto end;
+		}
+		krb525_hosts[0] = s;
+		krb525_hosts[1] = NULL;
+	}
+#endif
+
+	if (!krb525_hosts || !krb525_hosts[0]) {
+		retval = KRB5_KDC_UNREACH;
+		goto end;
+	}
+
+	retval = parse_krb525_hosts(krb525_hosts, port, use_port, krb525_endpoints);
+	if (retval)
+		goto end;
+
+	retval = 0;
+
+end:
+	if (krb525_hosts)
+		free_strings(krb525_hosts);
+	if (default_realm)
+		free(default_realm);
+
+	return (retval);
+}
 
 static krb5_error_code
 krb525_do_convert(krb5_context context,
@@ -437,6 +481,23 @@ krb525_do_convert(krb5_context context,
   return(0);
 }
 
+static int
+update_err_msg(char **err_msg, const char *fmt, ...)
+{
+	int ret;
+	char *s;
+	va_list ap;
+
+	va_start(ap, fmt);
+	ret = vasprintf(&s, fmt, ap);
+	va_end(ap);
+	if (ret == -1)
+		return ret;
+
+	*err_msg = s;
+	return 0;
+}
+
 krb5_error_code
 krb525_convert_with_ccache(krb5_context context,
 			   char         **hosts,
@@ -447,35 +508,78 @@ krb525_convert_with_ccache(krb5_context context,
 			   krb5_creds   *out_creds)
 {
   int             sock;
-  char           *krb525_host;
   krb5_creds     *krb525_creds;
   krb5_error_code retval;
-#ifdef HEIMDAL
-  krb5_realm      *realm;
-#else
-  krb5_data       *realm;
-#endif
+  char *realm;
   krb525_authdata   auth_data;
+  krb525_endpoint_t **krb525_endpoints = NULL;
+  krb525_endpoint_t **ep = NULL;
+  char *tmp_err_msg = NULL;
 
-  realm = &in_creds->server->realm;
+#ifdef HEIMDAL
+  realm = in_creds->server->realm;
+#else
+  realm = in_creds->server->realm.data;
+#endif
 
-  /* Try to contact server and get server name */
-  if(retval=krb525_connect(context, realm, hosts, port, &sock, &krb525_host))
-    return(retval);
+  if (hosts)
+	  retval = parse_krb525_hosts(hosts, port, 0, &krb525_endpoints);
+  else
+	  retval = get_krb525_endpoints(context, port, realm, &krb525_endpoints);
+  if (retval) {
+	  snprintf(krb525_convert_error, sizeof(krb525_convert_error), "failed to find krb525 hosts\n");
+	  return retval;
+  }
 
   auth_data.data = in_creds->authdata;
 
-  /* Get credentials for krb525d at the contacted host */
-  if(retval=get_krb525_creds_ccache(context, ccache, cname, krb525_host, 
-				    &auth_data, &krb525_creds)) {
-    close(sock);
-    return(retval);
+  retval = -1;
+  for (ep = krb525_endpoints; ep && *ep; ep++) {
+	  sock = connect_to_server((*ep)->hostname, (*ep)->port);
+	  if (sock < 0) {
+		  update_err_msg(&tmp_err_msg, "%s" "Failed to connect to server %s (%s)\n",
+				(tmp_err_msg) ? tmp_err_msg : "",
+				(*ep)->hostname, error_message(errno));
+		  retval = -1;
+		  continue;
+	  }
+
+	  retval = get_krb525_creds_ccache(context, ccache, cname, (*ep)->hostname,
+			  &auth_data, &krb525_creds);
+	  if (retval) {
+		  update_err_msg(&tmp_err_msg, "%s" "Failed to get credentials for server %s (%s)\n",
+				  (tmp_err_msg) ? tmp_err_msg : "",
+				  (*ep)->hostname, krb525_convert_error);
+		  close(sock);
+		  continue;
+	  }
+
+	  retval = krb525_do_convert(context, sock, krb525_creds, in_creds, out_creds);
+	  if (retval) {
+		  update_err_msg(&tmp_err_msg, "%s" "Failed to convert credentials with %s (%s)\n",
+				  (tmp_err_msg) ? tmp_err_msg : "",
+				  (*ep)->hostname, krb525_convert_error);
+		  close(sock);
+		  continue;
+	  }
+
+	  close(sock);
+	  retval = 0;
+	  break;
   }
 
-  /* Convert credentials */
-  retval = krb525_do_convert(context, sock, krb525_creds, in_creds, out_creds);
+  if (retval) {
+	  snprintf(krb525_convert_error, sizeof(krb525_convert_error), "%s",
+			  (tmp_err_msg) ? tmp_err_msg : "Failed to contact k525 servers");
+	  goto end;
+  }
 
-  close (sock);
+end:
+  if (tmp_err_msg)
+	  free(tmp_err_msg);
+  if (krb525_endpoints)
+	  free_endpoints(krb525_endpoints);
+
   return retval;
 }
 
